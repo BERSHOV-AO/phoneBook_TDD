f3a1197755f72a01b1356d452aefb47b73fd63eb
BershovAleksey <bershov.aleksey@bk.ru>
2023-09-06 17:51:47 +0300

    Stub and tests for add

--- src/test/java/PhoneBookTest.java
+++ src/test/java/PhoneBookTest.java
@@ -1,2 +1,13 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
 public class PhoneBookTest {
+
+    @Test
+    public void testAdd() {
+        PhoneBook phoneBook = new PhoneBook();
+        assertEquals(1, phoneBook.add("Мария", "89500000001"));
+        assertEquals(2, phoneBook.add("Алексей", "89500000002"));
+        assertEquals(2, phoneBook.add("Алексей", "89500000055"));
+    }
 }
